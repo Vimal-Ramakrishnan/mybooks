44196ce1ca301c660398af02320958c461520cd2
Vimal-Ramakrishnan <vimal@freshdesk.com>
2016-10-22 16:11:10 +0530

    Initial Commit

---
+++ tests/unit/routes/story-of-frontend-engineer-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:story-of-frontend-engineer', 'Unit | Route | story of frontend engineer', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
